2b7c497d48c7ae4261a0df9b9edb13434ed982c8
Robert Bubnik <rbubnik@gmail.com>
2023-09-16 15:25:01 -0400

    passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Your code here
+class Cat {
+    constructor (name, sex){
+        this.name = name;
+        this.sex = sex,
+        this.language = `meow`,
+        this.species = `cat`,
+        this.speak = () => {
+            
+            if (this.sex === `female`){
+            return `${this.name} says ${this.language}!`
+            }
+            else {
+              return  `$It's me! ${this.name}, the ${this.species}`
+            }
+    }
+  }
+}
+
+class Dog {
+    constructor (name, sex){
+        this.name = name;
+        this.sex = sex,
+        this.language = `woof`,
+        this.species = `dog`,
+        this.speak = () => {
+            
+            if (this.sex === `male`){
+            return `${this.name} says ${this.language}!`
+            }
+            else {
+              return  `It's me! ${this.name}, the ${this.species}!`
+            }
+    }
+  }
+}
+
+class Bird {
+    constructor (name, sex){
+        this.name = name;
+        this.sex = sex,
+        this.language = `squawk`,
+        this.species = `parrot`,
+        this.speak = () => {
+            
+            if (this.sex === `female`){
+            return `${this.name} says ${this.language}!`
+            }
+            else {
+              return  `It's me! ${this.name}, the ${this.species}!`
+            }
+    }
+  }
+}
